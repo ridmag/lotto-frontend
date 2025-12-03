28cc789219ed8927c80cd4073c76f8ebaacf11e3
Ridma Gamage <ridmag@gmail.com>
2025-12-03 15:22:42 +1300

    initial frontend commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template><div><h1>Lotto Number Predictor</h1><LottoForm /></div></template>
+<script>
+import LottoForm from './components/LottoForm.vue'
+export default { components: { LottoForm } }
+</script>
---
+++ src/components/LottoForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Enter last draw numbers</h2>
+    <input v-for="i in 6" :key="i" v-model.number="lastDraw[i-1]" type="number" min="1" max="40"/>
+    <h3>Optional numbers</h3>
+    <input v-for="i in 5" :key="i" v-model.number="userNumbers[i-1]" type="number" min="1" max="40"/>
+    <button @click="getPrediction">Predict</button>
+    <div v-if="top5.length"><h3>Top 5 per slot:</h3><div v-for="(slot, i) in top5" :key="i">{{ slot.join(', ') }}</div></div>
+    <div v-if="sets.length"><h3>Predicted sets:</h3><div v-for="(s, i) in sets" :key="i">{{ s.join(', ') }}</div></div>
+    <div class="adsense">
+      <ins class="adsbygoogle" style="display:block" data-ad-client='ca-pub-XXXXXXXXXX' data-ad-slot='YYYYYYYYYY' data-ad-format='auto' data-full-width-responsive='true'></ins>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return { lastDraw: Array(6).fill(1), userNumbers: Array(5).fill(1), top5: [], sets: [] }
+  },
+  methods: {
+    async getPrediction() {
+      const res = await axios.post('https://your-backend-url.onrender.com/predict', { last_draw: this.lastDraw, user_numbers: this.userNumbers })
+      this.top5 = res.data.top5_per_slot
+      this.sets = res.data.predicted_sets
+      (window.adsbygoogle = window.adsbygoogle || []).push({})
+    }
+  }
+}
+</script>
